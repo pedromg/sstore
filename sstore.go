82a6ecd2708cb936290e6962febcfab47c0eb4bc
pedro mg <pedro@tquadrado.com>
2014-01-24 20:48:50 +0000

    Working on ReadFile function.

--- sstore.go
+++ sstore.go
@@ -4,9 +4,10 @@ package sstore
 
 import (
 	"io/ioutil"
+	"strings"
 )
 
-type sStore struct {
+type SStore struct {
 	Name	string
 	Descr	string
 	Data	map[string]string
@@ -14,13 +15,13 @@ type sStore struct {
 
 // CreateFile creates a file with name, inserts the firstline descriptive text 
 // on the first line, and then the data key:value map of strings.
-func CreateFile(s sStore) error {
+func CreateFile(s SStore) error {
 	// set the map 
 	var d []byte
 	d = append(d, s.Descr...)
 	d = append(d, "\n"...)
 	d = append(d, map2bytes(s.Data)...)
-	err := ioutil.WriteFile(s.Name, d, 1)
+	err := ioutil.WriteFile(s.Name, d, 0777)
 	if err != nil {
 		return err
 	}
@@ -34,4 +35,21 @@ func map2bytes(m map[string]string) []byte {
 		b = append(b, "\n"...)
 	}
 	return b
+}
+
+func ReadFile(f string) (SStore, error) {
+	var the_store SStore
+	the_store.Name = f
+	content, error := ioutil.ReadFile(the_store.Name)
+	if error != nil {
+		return nil, error
+	}
+	lines := strings.Split(string(content), "\n")
+	the_store.Descr = lines[0]
+	for l := range lines {
+		the_l := strings.Split(l,":")
+		the_store.Data[l][0] = the_l[0]
+		the_store.Data[l][1] = the_l[1]
+	}
+	return the_store, nil
 }
--- sstore_test.go
+++ sstore_test.go
@@ -1,25 +1,49 @@
 package sstore
 
 import (
-	"fmt"
 	"testing"
 	"github.com/pedromg/sstore"
 )
 
-func TestCreateFile(t *testing.T) {
-	// create test values
-	var the_store sstore.sStore
-	the_store.Name = "test_file_sstore.ss"
-	the_store.Descr = "This defs file for test purposes."
-	the_store.Data = map[string]string{
+const	fn = "test_file_sstore.sstore"
+const	dsc = "This defs file for test purposes."
+var	data = map[string]string{
 		"aaa":"111",
 		"bbb":"222",
 		"ccc":"333",
 	}
+
+func TestCreateFile(t *testing.T) {
+	// create test values
+	var the_store sstore.SStore
+	the_store.Name = fn
+	the_store.Descr = dsc
+	the_store.Data = data
+	// the_store.Data = map[string]string{
+	// 	"aaa":"111",
+	// 	"bbb":"222",
+	// 	"ccc":"333",
+	// }
 	// create the file
-	if err := sstore.CreateFile(the_store); err != nil {
-		t.Error("Error creating the file.")
+	err := sstore.CreateFile(the_store);
+	if err != nil {
+		t.Error("Error: TestCreateFile test failed!")
+	} else {
+		t.Log("TestCreateFile test passed!")
 	}
 
 }
 
+func TestReadFile(t *testing.T) {
+	var the_store sstore.SStore
+	the_store, err := sstore.ReadFile(fn)
+	if err!= nil {
+		t.Error("Error: TestReadFile test failed!")
+	} else {
+		if (the_store.Name == fn) && (the_store.Descr == dsc) {
+			t.Log("TestReadFile test passed!")
+		} else {
+			t.Error("Error: TestReadFile test conditions failed!")
+		}
+	}
+}
